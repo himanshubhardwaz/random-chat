6ccddc57ebc28f6f8f5e9d01f76c9565d23d37c5
Himanshu Bhardwaz <himanshu76200@gmail.com>
2022-09-30 23:04:53 +0530

    updated structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <p className='text-red-500'>Hello</p>
+    </div>
+  );
+};
+
+export default Home;
