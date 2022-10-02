ad343bc7227ecfdb64d5e0dceae9aa56d5a04d83
Himanshu Bhardwaz <himanshu76200@gmail.com>
2022-10-02 22:56:55 +0530

    removed dehydrated state react query --todo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import "../styles/globals.css";
+import { Session } from "next-auth";
 import { useState } from "react";
 import type { AppProps } from "next/app";
 import {
@@ -8,13 +9,16 @@ import {
 } from "@tanstack/react-query";
 import { SessionProvider } from "next-auth/react";
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps<{ session: Session }>) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
     <SessionProvider session={session} refetchInterval={5 * 60}>
       <QueryClientProvider client={queryClient}>
-        <Hydrate state={pageProps?.dehydratedState}>
+        <Hydrate>
           <Component {...pageProps} />
         </Hydrate>
       </QueryClientProvider>
